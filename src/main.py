449c6d41e5ef509ad35e19d0c1dbf6b6a9ed41c1
h.kamiyo <h.kamiyo@april-knights.com>
2020-09-26 22:16:18 +0900

    Fix: 自分が設定したSTEP1の処理まで終わった
    TODO: まだテストコードが残ってる

--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
 import os
+import re
+from pprint import pprint
 
 from flask import Flask
 import requests
-import slack
+from slack import WebClient
 from slackeventsapi import SlackEventAdapter
 
 app = Flask(__name__)
+slack_signing_secret = os.environ["SIGINING"]
+slack_events_adapter = SlackEventAdapter(slack_signing_secret, "/slack/events", app)
+
+slack_bot_token = os.environ["PYDOC_TOKEN"]
+slack_client = WebClient(slack_bot_token)
 
 
 class SearchDocModule:
     def __init__(self, module_name):
-        self.client = slack.WebClient(token=os.environ["SLACK_API_TOKEN"])
         self.module_name = module_name
         self.url = f"https://docs.python.org/ja/3/library/{self.module_name}.html"
 
@@ -22,21 +28,31 @@ class SearchDocModule:
         return response
 
 
-slack_events_adapter = SlackEventAdapter(os.environ["SIGINING"], "/search/docs", app)
-
-
 @slack_events_adapter.on("message")
-def main(request):
-    app.logger.debug(request)
-    # sdm = SearchDocModule(request)
+def main(event_data):
+    message = event_data["event"]
+    channel = message["channel"]
+    # pprint(message)
+    if message.get("subtype") is None and re.match(r"^py\s", message.get("text")):
+        text = message.get("text").split()[1]
+        pprint(text)
+        sdm = SearchDocModule(text)
 
-    # if not sdm.check_str():
-    #     return "NG"
-
-    # if sdm.check_doc_url().status_code == 200:
-    #     return "OK"
+        status_code = sdm.check_doc_url().status_code
+        if status_code == 200:
+            print("success", status_code)
+            slack_client.chat_postMessage(channel=channel, text=sdm.url)
+        else:
+            print("statucode not 200", status_code)
+            slack_client.chat_postMessage(
+                channel=channel, text="The module name is incorrect"
+            )
     # else:
-    #     return "NG"
+    #     print("not match")
+    #     slack_client.chat_postMessage(
+    #         channel=channel, text="Command is [py space modulename]"
+    #     )
+    # ここ使うとbotが投稿した内容に反応して延々とぐるぐるするので要検討
 
 
 if __name__ == "__main__":
